4ac07e0defc2d7d1c24d46fc45d6ac7aecadeec6
Tamar <tamaraltahan@gmail.com>
2020-01-09 01:47:36 -0800

    idk

--- 330Lab1/330Lab1.cpp
+++ 330Lab1/330Lab1.cpp
@@ -8,12 +8,17 @@ using namespace std;
 
 
 void populateList(vector<int> vect, int n); //fill values with n random values
-int calculateMedian(vector<int> vect);
+int calculateMedian(vector<int> vect); //calculate Median value
+int recursivePrint(vector<int> vect, int n); //print out values of list recursively
 
 int main()
 {
+    int size;
+    cout << "How big is the vector (odd)?\n";
+    cin >> size;
+
     vector<int> list;
-    populateList(list, 11);
+    populateList(list, size);
     int median = calculateMedian(list);
 
     cout << "The median value is " << median << endl;
@@ -33,20 +38,42 @@ void populateList(vector<int> vect, int n)
 
 int calculateMedian(vector<int> vect)
 {
-    assert(vect.size() % 2 != 0);
+    assert(vect.size() % 2 == 0);
 
     int half = vect.size() / 2;
     int median = 0;
-    for (int i = 0; i < vect.size; ++i) {
-        int greaterThan = 0;
-        for (int j = 0; j < vect.size; ++j) {
-            if (vect[j] > vect[i]) {
+    int greaterThan = 0;
+    int lessThan = 0;
+
+    for (int i = 0; i < vect.size(); ++i) 
+    {
+        for (int j = 0; j < vect.size(); ++j) 
+        {
+            if (vect[j] > vect[i]) 
+            {
                 greaterThan++;
+            }
+            else {
+                lessThan++;
+            }
         }
-        if (greaterThan == half) {
+        if (greaterThan == lessThan)
+        {
             median = vect[i];
             return median;
         }
     }
-    return median;
+    return INT_MAX;
+}
+
+int recursivePrint(vector<int> vect, int n)
+{
+    cout << vect[n] << endl;
+    if (n == 0) {
+        return vect[n];
+    }
+    else {
+        return recursivePrint(vect, n - 1);
+    }
+    return vect[n];
 }
